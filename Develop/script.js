3f795c4e9d9a04f1b66ba39946d78f125e4da174
Joshua Chu <jashya@gmail.com>
2020-07-08 22:58:35 -0400

    Commit final status

--- Develop/script.js
+++ Develop/script.js
@@ -2,16 +2,38 @@
 //2013-02-08 09:30  Y M D H M
 
 const curTime = moment();
+const dayStart = 0;
+const dayEnd = 24;
+let schedule = {};
 
 $('#currentDay').text(moment().format('dddd MMMM Do, YYYY, h:mm A'));
 
-console.log(moment().hour());
+//console.log(moment().hour());
 
-$(document).ready(function () {
-    for (var i = 6; i < 22; i++) {
+// (Attempts to) Load local schedule
+// If local storage can't be loaded, initializes a blank schedule
+function loadLocal() {
+    if (localStorage.getItem("schedule") === null) {
+        for (var i = dayStart; i < dayEnd; i++) {
+            schedule[i] = "";
+        }
+        localStorage.setItem("schedule", JSON.stringify(schedule));
+    } else {
+        schedule = JSON.parse(localStorage.getItem("schedule"));
+    }
+}
+
+function render() {
+    loadLocal();
+    console.log(schedule);
+
+    for (var i = dayStart; i < dayEnd; i++) {
+        
         let hour = i;
         let AMPM = 'AM';
         let relative;
+        let block = $("<div class='row'>");
+
         if (hour > 12) {
             hour -= 12;
             AMPM = 'PM';
@@ -23,8 +45,27 @@ $(document).ready(function () {
         } else if (i > curTime.hour()) {
             relative = 'future';
         }
-        let block = $("<div class='row'>");
-        $(block).append(`<div class='col ${relative}' id='${hour}${AMPM}'>${hour}:00 ${AMPM}</div>`);
+        
+        $(block).append(`
+            <div class='col-1 hour time-block'>
+                <p>${hour} ${AMPM}</p>
+            </div>
+            <div class='col description'>
+                <form>
+                    <div class="form-group ${relative}">
+                        <textarea class='form-control ' id='${hour}${AMPM}' placeholder="Enter details" rows="2"></textarea>
+                    </div>
+                </form>
+            </div>
+            <div class='col-1'>
+                <button type="button" class="btn saveBtn">Save</button>
+            </div>
+            
+            `);
         $(".container").append(block);
     }
+}
+
+$(document).ready(function () {
+    render();
 })
